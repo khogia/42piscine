0d14eee5e1dbc67122742c5cc15c732f5cd52e21
Khanh Ho Gia <kho-gia@c3r9s1.42prague.com>
2023-02-19 23:21:37 +0100

    fixed version of ft_strjoin

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -12,31 +12,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
 	int		j;
-	int		k;
-	int		l;
 	char	*str;
 
 	i = 0;
-	k = 0;
-	str = (char *)malloc(sizeof(strs));
-	while (size > i)
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
-		j = 0;
-		while (strs[i][j] != '\0')
-		{
-			str[k] = strs[i][j];
-			j++;
-			k++;
-		}
-		l = 0;
-		while (sep[l] != '\0' && i < size - 1)
-			str[k++] = sep[l++];
+		str[i] = s1[i];
 		i++;
 	}
-	str[k] = '\0';
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
 	return (str);
 }
